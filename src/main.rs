0f46d4db8009d49e38297df4b1292661cb61c37d
Syn-dromatic <68112904+Syn-dromatic@users.noreply.github.com>
2023-05-03 12:16:33 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,32 @@ mod timer_module;
 
 use test::algorithm_test::{binary_search_value, generate_array};
 use timer_module::profiler::TimeProfiler;
+use timer_module::timer::TimerModule;
+
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
-    let mut profiler: TimeProfiler = TimeProfiler::new(true);
+    let mut profiler: TimeProfiler = TimeProfiler::new(false);
 
     profile_function(&mut profiler);
     profiler.print_profiling_report();
+
+    timer_example();
 }
 
 fn profile_function(profiler: &mut TimeProfiler) {
     let array: Vec<i32> = generate_array(10_000);
-    println!("STARTING");
-
     let mut function3 = profiler.function_wrapper(|(_, _)| binary_search_value);
 
     for value in array.iter() {
         function3((&array, *value));
     }
+}
+
+fn timer_example() {
+    let mut timer_module = TimerModule::new();
+    timer_module.start();
+    sleep(Duration::from_millis(100));
+    println!("Timer Example: {:?}", timer_module)
 }
